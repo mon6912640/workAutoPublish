c44c6a19df8f9537a51962989a0da18b90cf803f
mon6912640 <010monkey@163.com>
2019-05-20 16:43:42 +0800

    去除覆盖提示

--- v2/publish_main.bat
+++ v2/publish_main.bat
@@ -12,7 +12,7 @@ set RESOURCE_DIR=%PJ_DIR%\resource_Publish
 xcopy %RESOURCE_DIR%\default.res.json %RELEASE_DIR%\%VER_STR%\ /s/y/i
 xcopy %RESOURCE_DIR%\default.res.json %RELEASE_DIR%\resource\%VER_STR%\ /s/y/i
 xcopy %JS_DIR%\manifest.json %RELEASE_DIR%\%VER_STR%\ /s/y/i
-xcopy %JS_DIR%\js\*.* %RELEASE_DIR%\js\ /s/d/i
-xcopy %RESOURCE_DIR%\assets\*.* %RELEASE_DIR%\resource\assets\ /s/d/i
+xcopy %JS_DIR%\js\*.* %RELEASE_DIR%\js\ /s/y/d/i
+xcopy %RESOURCE_DIR%\assets\*.* %RELEASE_DIR%\resource\assets\ /s/y/d/i
 cd %PJ_DIR%
 pause
